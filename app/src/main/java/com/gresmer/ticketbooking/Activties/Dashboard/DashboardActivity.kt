8e3e225d6ab7e161f0294e435f00a934d0f208e2
Kevin Gresmer <kgresmer@gmail.com>
2025-04-17 12:08:10 -0500

    Creating bottom bar and shell of dashboard activity.

---
+++ app/src/main/java/com/gresmer/ticketbooking/Activties/Dashboard/DashboardActivity.kt
@@ -0,0 +1,43 @@
+package com.gresmer.ticketbooking.Activties.Dashboard
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.gresmer.ticketbooking.Activties.Splash.StatusTopBarColor
+import com.gresmer.ticketbooking.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MainScreen() {
+    StatusTopBarColor()
+    Scaffold(
+        bottomBar = { MyBottomBar() },
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues=paddingValues)
+                .background(color = colorResource(R.color.darkPurple2))
+        ) { }
+    }
+}
